cca351eaf1925327e99bd41be5078c1101656ec2
Gary Moore <littlebunch@gmail.com>
2021-11-16 14:45:42 -0500

    added enum.go

---
+++ model/enum.go
@@ -0,0 +1,100 @@
+// Package fdc describes food products data model
+package bfpd
+
+// DocType provides a list of document types
+type DocType int
+
+// FULL etc define values for Foods formats
+const (
+	FULL      = "full"
+	META      = "meta"
+	SERVING   = "servings"
+	NUTRIENTS = "nutrients"
+)
+
+// PHRASE etc defines values for Search Types
+const (
+	PHRASE   = "PHRASE"
+	WILDCARD = "WILDCARD"
+	REGEX    = "REGEX"
+)
+
+// SR is standard reference
+const (
+	SR DocType = iota
+	FGSR
+	FNDDS
+	FGFNDDS
+	FGGPC
+	BFPD
+	UNIT
+	NUT
+	DERV
+	FOOD
+	USER
+	NUTDATA
+)
+
+//ToDocType -- convert a string to a DocType
+func (dt *DocType) ToDocType(t string) DocType {
+	switch t {
+	case "SR":
+		return SR
+	case "FGSR":
+		return FGSR
+	case "FGFNDDS":
+		return FGFNDDS
+	case "FGGPC":
+		return FGGPC
+	case "FNDDS":
+		return FNDDS
+	case "BFPD":
+		return BFPD
+	case "UNIT":
+		return UNIT
+	case "NUT":
+		return NUT
+	case "NUTDATA":
+		return NUTDATA
+	case "DERV":
+		return DERV
+	case "FOOD":
+		return FOOD
+	case "USER":
+		return USER
+	default:
+		return 999
+	}
+}
+
+//ToString -- convert a DocType to a string
+func (dt *DocType) ToString(t DocType) string {
+	switch t {
+	case SR:
+		return "SR"
+	case FGSR:
+		return "FGSR"
+	case FGFNDDS:
+		return "FGFNDDS"
+	case FNDDS:
+		return "FNDDS"
+	case FGGPC:
+		return "FGGPC"
+	case BFPD:
+		return "BFPD"
+	case UNIT:
+		return "UNIT"
+	case NUT:
+		return "NUT"
+	case NUTDATA:
+		return "NUTDATA"
+	case DERV:
+		return "DERV"
+	case FOOD:
+		return "FOOD"
+	case USER:
+		return "USER"
+	default:
+		return ""
+	}
+}
